c6b662c81953994661b9d75595b5be826401074b
RaisuFire <wodel33@outlook.com>
2020-09-17 21:14:47 +0800

    1

--- DesignOfPrograms/lesson3/NAryFunction.py
+++ DesignOfPrograms/lesson3/NAryFunction.py
@@ -4,6 +4,20 @@
 # Write a function, n_ary(f), that takes a binary function (a function
 # that takes 2 inputs) as input and returns an n_ary function.
 
+from functools import update_wrapper
+
+
+def decorator(d):
+    "make function d a decorator : d wraps a function fn."
+
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+
+    update_wrapper(_d, d)
+    return _d
+
+
+@decorator
 def n_ary(f):
     """Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
@@ -15,3 +29,13 @@ def n_ary(f):
             return f(x, n_ary_f(*args))
 
     return n_ary_f
+
+
+@n_ary
+def seq(x, y):
+    return ('seq', x, y)
+
+
+# seq = n_ary(seq)
+
+help(seq)
