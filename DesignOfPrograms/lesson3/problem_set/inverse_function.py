12886ee402f3b2107ce63536b77e1d6e434647e9
RaisuFire <wodel33@outlook.com>
2020-09-25 08:33:15 +0800

    1

--- DesignOfPrograms/lesson3/problem_set/inverse_function.py
+++ DesignOfPrograms/lesson3/problem_set/inverse_function.py
@@ -52,10 +52,31 @@ def slow_inverse(f, delta=1 / 128.):
     return f_1
 
 
-def inverse(f, delta=1 / 128.):
-    """Given a function y = f(x) that is a monotonically increasing function on
-    non-negatve numbers, return the function x = f_1(y) that is an approximate
-    inverse, picking the closest value to the inverse, within delta."""
+def find_bounds(f, y):
+    x = 1.
+    while f(x) < y:
+        x = x * 2
+    lo = 0 if (x == 1) else x/2
+    return lo, x
+
+
+def binary_search(f, y, lo, hi, delta):
+    while lo <= hi:
+        x = (lo + hi) / 2
+        if f(x) < y:
+            lo = x + delta
+        elif f(x) > y:
+            hi = x - delta
+        else:
+            return x
+    return hi if (f(hi)-y < y -f(lo)) else lo
+
+
+def inverse(f, delta=1/1024.):
+    def f_1(y):
+        lo, hi = find_bounds(f, y)
+        return binary_search(f, y, lo, hi, delta)
+    return f_1
 
 
 def square(x): return x * x
