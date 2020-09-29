e460858db3d6b6241880c1ba7dfeffddebcd7699
RaisuFire <wodel33@outlook.com>
2020-09-29 16:15:10 +0800

    1

--- DesignOfPrograms/lesson4/BridgeSuccessors.py
+++ DesignOfPrograms/lesson4/BridgeSuccessors.py
@@ -28,11 +28,6 @@ def bsuccessors(state):
         here = here.remove(s, 'light')
         item = {(here.remove(s), here.remove('light')), ()}
 
-
-
-
-
-
 def test():
     assert bsuccessors((frozenset([1, 'light']), frozenset([]), 3)) == {
         (frozenset([]), frozenset([1, 'light']), 4): (1, 1, '->')}
